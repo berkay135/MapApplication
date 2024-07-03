45a1b167b7b26bb92d760be1b5ef7bd2f698be03
berkay135 <yesildereberkay@gmail.com>
2024-07-03 17:15:51 +0300

    -crud operations added

---
+++ Controllers/PointController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MapApplication.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PointController : ControllerBase {
+
+        private static readonly List<Point> points = new List<Point>();
+
+        [HttpGet]
+
+        public List<Point> GetAll() {
+            return points;
+        }
+
+        [HttpGet("GetById")]
+        public ActionResult<Point> GetById(int Id) {
+            if (Id <= 0) {
+                return NotFound("Invalid Id!");
+            }
+
+            Point point = points.FirstOrDefault(u => u.Id == Id);
+            if (point == null) {
+                return NotFound("Not Found");
+            }
+
+            return point;
+
+        }
+
+        
+        [HttpPost("Update")]
+        public ActionResult Update(Point point) {
+
+            var item = points.FirstOrDefault(u => u.Id == point.Id);
+
+            if (item == null) {
+
+                return NotFound();
+            }
+
+            item.X = point.X;
+            item.Y = point.Y;
+            item.Name = point.Name;
+
+            return Ok(item);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public ActionResult Delete(int id) {
+
+            var item = points.FirstOrDefault(u => u.Id == id);
+
+            if (item == null) {
+
+                return NotFound();
+            }
+
+            points.Remove(item);
+
+            return Ok($"Deleted Item's Name: {item.Name}");
+        }
+
+        [HttpPost]
+        public Point Add(Point point) {
+            var item = new Point();
+
+            item.Id = point.Id;
+            item.X = point.X;
+            item.Y = point.Y;
+            item.Name = point.Name;
+            points.Add(item);
+
+            return item;
+        }
+
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,8 @@
+﻿namespace MapApplication {
+    public class Point {
+        public int Id { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string Name { get; set; }
+    }
+}
